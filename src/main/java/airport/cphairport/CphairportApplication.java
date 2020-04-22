5dd9c70e8b207cf8600ed82b613c974574aff279
HansenKenneth <arme@taxastork.dk>
2020-04-22 13:52:59 +0200

    Initial commit

---
+++ src/main/java/airport/cphairport/CphairportApplication.java
@@ -0,0 +1,13 @@
+package airport.cphairport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CphairportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CphairportApplication.class, args);
+    }
+
+}
